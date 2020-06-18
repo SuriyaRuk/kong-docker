1f15d60f597e2f9c2ac74da1f0504a753018de38
poc-tb-cert-kong <root@poc-tb-cert-kong>
2020-06-19 02:33:52 +0700

    test-break-down

--- start.sh
+++ start.sh
@@ -13,14 +13,32 @@ do
     sleep 5
 done
 
+docker-compose up -d proxy-database
+
+STATUS_PROXY="starting"
+
+while [ "$STATUS_PROXY" != "healthy" ]
+do
+    STATUS_PROXY=$(docker inspect --format {{.State.Health.Status}} proxy-database)
+    echo "proxy-database state = $STATUS_PROXY"
+    sleep 5
+done
 
 echo "Run database migrations..."
 
 docker-compose up migrations
 
+
+echo "Run database proxy-migrations..."
+
+docker-compose up proxy-migrations
+
 echo "Starting kong..."
 
-docker-compose up -d kong
+docker-compose up -d kong1
+docker-compose up -d kong2
+docker-compose up -d proxy
 
-echo "Kong admin running http://127.0.0.1:8001/"
+#echo "Kong admin running http://127.0.0.1:8001/"
 
+docker-compose up -d konga
